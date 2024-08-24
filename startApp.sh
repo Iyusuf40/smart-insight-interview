df67e9307b0389d8280779973a7de91d030f564b
iyusuf <punkgts40@gmail.com>
2024-08-24 15:11:01 +0100

    setup db in startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+service redis-server start
+service mysql start
+
+# Set the database name, username, and password
+DB_NAME="tweetAi"
+USERNAME="tweetAiAdmin"
+PASSWORD="tweetAiPassword"
+
+# Create the database
+sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+# Create the user and grant privileges
+sudo mysql -u root -e "CREATE USER IF NOT EXISTS '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USERNAME'@'%';"
+
+# Flush privileges to apply the changes
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
+
+# start api
 node api/server.js &
 
 CURRENT_DIR=$(pwd)
